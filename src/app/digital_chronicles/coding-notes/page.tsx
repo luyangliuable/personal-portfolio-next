3c3ef8c104ce5de85242bd1d3a9063aaa264d997
Liu <Lucas.Liu@cba.com.au>
2024-08-01 16:26:21 +1000

    Made notes page use accordions.

--- src/app/digital_chronicles/coding-notes/page.tsx
+++ src/app/digital_chronicles/coding-notes/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React, { useEffect } from "react";
-import BlogPage from "../../../page/BlogPage/BlogPage";
 import type { Metadata } from "next";
 import HeroHeader from "../../../components/HeroHeader/HeroHeader";
 import { useGetNoteListQuery } from "../../../stores/Repository/Notes";
 import Card from "../../../components/Card/Card";
-import {Accordion} from "../../../components/Accordion/Accordion";
+import { Accordion } from "../../../components/Accordion/Accordion";
+import BlogPostResponse from "../../../repositories/Response/BlogPostResponse";
 
 /* export const metadata: Metadata = {
  *     title: "Luyang's Coding Notes",
@@ -14,49 +14,66 @@ import {Accordion} from "../../../components/Accordion/Accordion";
  * }; */
 
 const BlogPageWithTopPicks: React.FC = () => {
-  const heroHeaderContent = Object.freeze({
-    heading: "Coding Notes",
-    description: "My personal coding notes and snippets, organized by language and topic. The approach is by helping you learn by looking at examples."
-  });
+    const heroHeaderContent = Object.freeze({
+        heading: "Coding Notes",
+        description: "My personal coding notes and snippets, organized by language and topic that I wrote during the nostalgic days at University."
+    });
 
-  const { data, error, isLoading } = useGetNoteListQuery();
+    const { data, error, isLoading } = useGetNoteListQuery();
 
-  useEffect(() => {
-    console.log(data);
-  }, [])
+    useEffect(() => {
+        console.log(data);
+    }, [])
 
-  if (isLoading) return (<></>);
-  if (error) return <div>Error</div>;
+    if (isLoading) return (<></>);
+    if (error) return <div>Error</div>;
 
-  const authorImage = "https://llcode.tech/api/image/65817ae96c73ceb16ba51731";
-  return (
-    <main>
-      <HeroHeader heading={heroHeaderContent.heading} description={heroHeaderContent.description} />
-      <Accordion>
-        <Accordion.Item heading="Agile Methodology">
-          sadasd
-        </Accordion.Item>
-      </Accordion>
-      {false && data!.map(content => {
-        return (
-          <Card
-            key={content._id.$oid}
-            heading={content.heading}
-            authorImage={authorImage}
-            author={content.author}
-            date_created={content.date_created}
-            date_updated={content.date_last_modified}
-            body={content.body}
-            minuteRead={content.reading_time_minutes}
-            in_progress={content.in_progress}
-            tags={content.tags}
-            image={content.image && content.image.$oid}
-            link={`/digital_chronicles/coding-note/${content._id.$oid}`}
-          />
-        )
-      })}
-    </main>
-  );
+    const grouped = data!.reduce((acc: Record<string, any>, item: BlogPostResponse) => {
+        const tagName = item.tags.length > 0 ? item.tags[0] : "random";
+
+        if (acc.hasOwnProperty(tagName)) {
+            acc[tagName].push(item);
+        } else {
+            acc[tagName] = [item];
+        }
+
+        return acc;
+    }, {});
+
+    const authorImage = "https://llcode.tech/api/image/65817ae96c73ceb16ba51731";
+    return (
+        <main>
+            <HeroHeader heading={heroHeaderContent.heading} description={heroHeaderContent.description} />
+            <Accordion>
+                {
+                    Object.keys(grouped).map(category => {
+                        return (
+                            <Accordion.Item heading={category}>
+                                {grouped[category].map(( content: BlogPostResponse ) => {
+                                    return (
+                                        <Card
+                                            key={content._id.$oid}
+                                            heading={content.heading}
+                                            authorImage={authorImage}
+                                            author={content.author}
+                                            date_created={content.date_created}
+                                            date_updated={content.date_last_modified}
+                                            body={content.body}
+                                            minuteRead={content.reading_time_minutes}
+                                            in_progress={content.in_progress}
+                                            tags={content.tags}
+                                            image={content.image && content.image.$oid}
+                                            link={`/digital_chronicles/coding-note/${content._id.$oid}`}
+                                        />
+                                    )
+                                })}
+                            </Accordion.Item>
+                        )
+                    })
+                }
+            </Accordion>
+        </main>
+    );
 }
 
 export default BlogPageWithTopPicks;
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Accordion.css";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { cl } from "../Utility/LogicUtility";
 
 interface IAccordionItemProps {
- heading: string,
+    heading: string,
     children: React.ReactNode
 }
 
 const Item: React.FC<IAccordionItemProps> = ({ heading, children }) => {
+    const [show, setShow] = useState<boolean>(false);
+
     return (
         <div className="accordion--item flex flex-col">
-            <div className="accordion--tab flex items-center cursor-pointer">{heading}</div>
-            <div className="accordion--content">
+            <div onClick={() => setShow(prev => !prev)} className="accordion--tab noselect flex items-center justify-between cursor-pointer">
+                <span>{heading}</span>
+                <IoIosArrowDropdown className={cl({ "flip": show, "no-flip": !show })} />
+            </div>
+            <div className={cl("accordion--content w-full flex flex-col items-center", { "none": !show })}>
                 {children}
             </div>
         </div>

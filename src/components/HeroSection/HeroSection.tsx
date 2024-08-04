e383a04b8fa34bbf6e41eab23760e0ed8405ca19
Luyang Liu <luyang.l@proton.me>
2024-08-04 21:24:08 +1000

    Scale not scaleX to prevent spaghetification.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -130,7 +130,7 @@ const HeroSection: React.FC<IHeroProps> = ({}) => {
                     opacity: clamp(.5, 1, 1000 - (scrollY ?? 0), 1 / 1000),
                     /* borderRadius: `${clamp(12, 300, scrollY, 1 / 2)}px`, */
                     /* width: `calc(98vw - ${clamp(0, 50, (scrollY ?? 0), 1 / 6)}px`, */
-                    transform: `scaleX(${clamp(.8, 1, 3200 - (scrollY ?? 0), 1 / 3200)}) translateY(${clamp(-260, 0, -(scrollY ?? 0), 1 / 2)}px)`
+                    transform: `scale(${clamp(.8, 1, 3200 - (scrollY ?? 0), 1 / 3200)}) translateY(${clamp(-260, 0, -(scrollY ?? 0), 1 / 2)}px)`
                 }}
                 className="hero-section" landingPageCardType="fitContent" >
                 <div className="space h-28"></div>

7c59173531bff355a70d59080b4c8373663831ac
Luyang Liu <luyang.l@proton.me>
2024-08-06 18:25:46 +1000

    Refactor code to use gsap for some animation and timeline scroll.

--- src/components/Atoms/SequentialRiseSpan2/SequentialRiseSpan2.tsx
+++ src/components/Atoms/SequentialRiseSpan2/SequentialRiseSpan2.tsx
@@ -1,68 +1,16 @@
-import "../SequentialRiseSpan/SequentialRiseSpan.css";
 import { ISequentialRiseSpanProps } from "../SequentialRiseSpan/SequentialRiseSpan";
 import React from "react";
 
 const SequentialRiseSpan2: React.FC<ISequentialRiseSpanProps> = ({
-    children,
-    elementType = "p",
-    wordsPerAnimation = 5,
-    animationDelayMiliseconds = 70,
-    className
+  children,
+  className
 }) => {
 
-    const setWrappedLines = () => {
-        const lines = String(children).split(" ");
-
-        const lumpedLines: string[] = [];
-
-        let currentLine = '';
-
-        lines.forEach((word) => {
-            const space = currentLine ? ' ' : '';
-            if ((currentLine + space + word).split(" ").length > wordsPerAnimation) {
-                lumpedLines.push(currentLine);
-                currentLine = word;
-            } else {
-                currentLine += (currentLine.length > 0 ? ' ' : '') + word;
-            }
-        })
-
-        if (currentLine) {
-            lumpedLines.push(currentLine);
-        }
-
-        const lineElements = lumpedLines.map((line, index) => {
-            const content = line + "\u00A0";
-            const LineElement = React.createElement(
-                'span',
-                {
-                    key: index,
-                    style: { animationDelay: `${index * animationDelayMiliseconds}ms`},
-                    className: ["visible-hidden", "slide-up"].join(" ")
-                },
-                content
-            );
-            return LineElement;
-        });
-
-        return lineElements;
-    };
-
-    const lineElements = setWrappedLines();
-
-    return (
-        <div className="sequential-rise-span">
-            {
-                React.createElement(
-                    elementType,
-                    { className: className },
-                    lineElements.map((line, _) => {
-                        return line
-                    })
-                )
-            }
-        </div>
-    )
+  return (
+    <div className={`sequential-rise-span ${className}`}>
+      {children}
+    </div>
+  )
 };
 
 export default SequentialRiseSpan2;
--- src/components/HeroHeader/HeroHeader.tsx
+++ src/components/HeroHeader/HeroHeader.tsx
@@ -1,70 +1,69 @@
-"use client";
-
-import React, { useEffect, useRef, useState } from "react";
+"use client"; import React, { useEffect, useRef } from "react";
 import IHeroHeaderProps from "./Interface/IHeroHeaderProps";
 import SequentialRiseSpan from "../Atoms/SequentialRiseSpan/SequentialRiseSpan";
-import { clamp } from "../Utility/LogicUtility";
 import "./HeroHeader.css";
-import { useScrollPosition } from "../../hooks";
+
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const HeroHeader: React.FC<IHeroHeaderProps> = ({ heading, description, graphics }) => {
-    const [isRendered, setIsRendered] = useState(false);
-
-    const [animationScroll, setAnimationScroll] = useState<number | null>(null);
-
-    const { scrollY } = useScrollPosition();
-
     const componentRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        console.log(animationScroll);
-    }, [animationScroll])
+        const heroHeader = {
+            this: ".hero-header",
+            graphics: ".hero-header__graphics-container",
+            content: ".hero-header__content",
+            heading: ".hero-header__heading",
+            description: ".hero-header__description"
+        };
 
-    useEffect(() => {
-        const addIntersectionObserver = () => {
-            const observer = new IntersectionObserver((entries) => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        const posY = componentRef.current!.getBoundingClientRect().top + window.scrollY;
-                        setAnimationScroll(posY < 100 ? 1 : posY);
-                    }
-                });
-            }, { threshold: [0.1] });
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: heroHeader.this,
+                start: "top top",
+                end: "bottom top",
+                scrub: true
+            },
+        });
 
-            if (componentRef.current) observer.observe(componentRef.current);
+        tl.add(gsap.to(heroHeader.this, {
+            opacity: .3,
+            transform: "translateY(-150px) scale(95%)",
+            borderBottomColor: "#333",
+        }), "start")
 
-            return observer;
-        }
-        const observer = addIntersectionObserver();
-        return () => observer.disconnect();
-    }, [componentRef]);
+        return () => {
+            tl.kill();
+        };
+    }, [])
 
     return (
         <div
             ref={componentRef}
-            className="hero-header flex flex-row justify-start items-center"
-            style={{
-                visibility: animationScroll ? "visible" : "hidden",
-                opacity: animationScroll ? clamp(.3, 1, 300 - ((scrollY ?? 0) - animationScroll), 1 / 300) : 1,
-                borderBottom: `${animationScroll ? clamp(.1, 2, (scrollY ?? 0) - animationScroll, 1 / 5) : ".1"}px solid #DDD`,
-                transform: animationScroll ? `translateY(${clamp(-150, 0, -((scrollY ?? 0) - animationScroll), 1 / 2)}px)` : "none",
-                width: animationScroll ? `${clamp(95, 100, 100 * 50 - ((scrollY ?? 0) - animationScroll), 1 / 50)}% ` : "100%"
-            }}>
+            className="hero-header flex flex-row justify-start items-center">
             {
                 graphics && (
                     <div className="hero-header__graphics-container">{graphics}</div>
                 )
             }
-            <div className="w-full">
-                <SequentialRiseSpan elementType="h1">
-                    {heading}
-                </SequentialRiseSpan>
-                <SequentialRiseSpan
-                    className="hero-header--description"
-                    maxNumberOfLettersPerLine={100}
-                    calculationAdjustment={.8}>
-                    {description}
-                </SequentialRiseSpan>
+            <div className="hero-header__content w-full">
+                <div className="hero-header__heading" >
+                    <SequentialRiseSpan elementType="h1">
+                        {heading}
+                    </SequentialRiseSpan>
+                </div>
+                <div>
+                    <div className="hero-header__description">
+                        <SequentialRiseSpan
+                            maxNumberOfLettersPerLine={100}
+                            calculationAdjustment={.8}>
+                            {description}
+                        </SequentialRiseSpan>
+                    </div>
+                </div>
             </div>
         </div >
     );

1ed4b6b1705513844e4a62a7c8bf36f5ee6c515b
Luyang Liu <luyang.l@proton.me>
2024-08-31 09:55:21 +1000

    UPdated navbar

--- src/components/Navbar/NavLink/NavLink.tsx
+++ src/components/Navbar/NavLink/NavLink.tsx
@@ -2,54 +2,96 @@
 
 import React from "react";
 import { NavbarItem } from "../Interface/INavbarState";
-import Link from "next/link";
 import { AiOutlineDown } from "react-icons/ai";
 import { CiLock } from "react-icons/ci";
+import { cl } from "../../Utility/LogicUtility";
 
 interface INavLinkProps {
-  link: NavbarItem,
-  isSubLink: boolean,
-  links?: NavbarItem[],
-  hideDropdownMenu: any,
-  renderDropdownMenu: any
+    link: NavbarItem,
+    isSubLink: boolean,
+    links?: NavbarItem[],
+    hideDropdownMenu: any,
+    renderDropdownMenu: any
 }
 
 import { usePathname } from 'next/navigation';
+import EmojIcon from "../../EmojIcon/EmojIcon";
 
 const NavLink: React.FC<INavLinkProps> = ({ link, isSubLink, renderDropdownMenu, links, hideDropdownMenu }) => {
-  // https://mikebifulco.com/posts/javascript-filter-boolean
+    // https://mikebifulco.com/posts/javascript-filter-boolean
 
-  const navLinkContent = [
-    link.name, link.icon,
-    link.sublinks && <AiOutlineDown key="down-icon" />,
-    link.isLocked && <CiLock key="lock-icon" />
-  ].filter(Boolean);
+    const navLinkContent = [
+        link.name,
+        link.sublinks && <AiOutlineDown key="down-icon" />,
+    ].filter(Boolean);
 
-  const targetPath = link.isLocked ? undefined : link.to;
+    const targetPath = link.isLocked ? undefined : link.to;
 
-  const onMouseOverAction = isSubLink && links ? () => {} : () => renderDropdownMenu(links?.filter(item => item.name === link.name)[0].sublinks);
+    const onMouseOverAction = isSubLink && links ? () => {} : () => renderDropdownMenu(links?.filter(item => item.name === link.name)[0].sublinks);
 
-  const pathname = usePathname() ?? "/";
+    const pathname = usePathname() ?? "/";
 
-  const isActive = (currentPathname?: string, targetPathname?: string) => {
-    if (!currentPathname || !targetPathname) return false;
-    if (currentPathname === targetPathname) return true;
-    if (targetPathname === "/") return false;
-    const currentPath = currentPathname.trim().replace(/\/+$/, "");
-    const normalizedTargetPath = targetPathname.trim().replace(/\/+$/, "");
-    return currentPath.startsWith(normalizedTargetPath);
-  };
+    const isActive = (currentPathname?: string, targetPathname?: string) => {
+        if (!currentPathname || !targetPathname) return false;
+        if (currentPathname === targetPathname) return true;
+        if (targetPathname === "/") return false;
+        const currentPath = currentPathname.trim().replace(/\/+$/, "");
+        const normalizedTargetPath = targetPathname.trim().replace(/\/+$/, "");
+        return currentPath.startsWith(normalizedTargetPath);
+    };
 
-  return (
-    <a
-      href={(targetPath && !link.isDisabled) ? targetPath : pathname}
-      onClick={() => link.isDisabled && hideDropdownMenu()}
-      className={`navbar-item flex justify-center items-center  ${isActive(pathname, targetPath) ? "active-link" : ""}`}
-      key={link.name}
-      onMouseOver={onMouseOverAction}>
-      {navLinkContent}
-    </a>
-  );
+    return (
+        <a
+            href={(targetPath && !link.isDisabled) ? targetPath : pathname}
+            onClick={() => link.isDisabled && hideDropdownMenu()}
+            className={cl("navbar-item flex flex-col justify-center items-center", {
+                "items-start": isSubLink,
+                "active-link": isActive(pathname, targetPath)
+            })}
+            key={link.name}
+            onMouseOver={onMouseOverAction}>
+
+            <div className="flex flex-row justify-center items-center">
+                {link.emoji && <EmojIcon style={{
+                    width: "40px",
+                    height: "40px",
+                    minWidth: "40px",
+                    minHeight: "40px",
+                    borderRadius: "4px",
+                    marginRight: ".5rem"
+                }} emojis={[link.emoji]} />}
+                <div className="flex flex-col justify-center">
+                    <div className="flex flex-row items-center">
+                        {navLinkContent}
+                    </div>
+                    {
+                        isSubLink && <div style={{
+                            fontSize: ".8rem",
+                            marginTop: "2px",
+                            color: "#888"
+                        }}>{link.description}</div>
+                    }
+                </div>
+                {
+                    link.isLocked &&
+                    <div
+                        className="blur-boundary position-absolute flex justify-center items-center"
+                        style={{
+                            background: "rgba(255,255,255,.5)",
+                            width: "30px",
+                            height: "30px",
+                            borderRadius: "4px",
+                            right: "10%",
+                            border: ".1px solid #888",
+                            transform: "translateY(10px)"
+                        }}>
+                        <CiLock
+                            key="lock-icon" />
+                    </div>
+                }
+            </div>
+        </a>
+    );
 };
 
 export default NavLink;

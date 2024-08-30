c1f2ea0a8fe435d726d279bbe32d8c31bfb8ddf6
Luyang Liu <luyang.l@proton.me>
2024-08-30 13:54:59 +1000

    Fix footer main margin and burger menu shows on large screens.

--- src/components/Navbar/NavBurgerPanel/NavBurgerPanel.tsx
+++ src/components/Navbar/NavBurgerPanel/NavBurgerPanel.tsx
@@ -4,10 +4,11 @@ import React, { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 import Link from "next/link";
 import INavBurgerPanelProps from "./Interface/INavBurgerPanelProps";
-import "./NavBurgerPanel.css";
 import { usePathname } from 'next/navigation';
 import Accordion from '../../Accordion/Accordion';
 import { ILink, NavbarItem } from '../Interface/INavbarState';
+
+import "./NavBurgerPanel.css";
 
 const NavBurgerPanel: React.FC<INavBurgerPanelProps> = ({ burgerPanel, links }) => {
   const pathname = usePathname();

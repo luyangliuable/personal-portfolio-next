f0b2398d62d46fa55a38d96ce543bc40ccffc1cc
Luyang Liu <luyang.l@proton.me>
2024-09-14 23:29:46 +1000

    Restore `next.config.mjs` and gallery item type z-index fix.

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-        // Add support for importing images
-        config.module.rules.push({
-            test: /\.(png|jpe?g|gif|svg|webp)$/i, // Regex for matching image file types
-            type: 'asset/resource', // This handles images as resources
-        });
-
-        return config;
-    },
-    images: {
-        // Configure the domains you want to allow images from, if needed
-        domains: [], // Add domains here if you need to load images from external sources
-        // Optionally specify the allowed image formats
-        formats: ['image/avif', 'image/webp'],
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;

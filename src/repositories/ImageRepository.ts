3b773f7cfca0c2acd40dd7d18e1ca4f8a5fb6412
Luyang Liu <luyang.l@proton.me>
2024-09-19 23:46:11 +1000

    Added cache control

--- src/repositories/ImageRepository.ts
+++ src/repositories/ImageRepository.ts
@@ -24,7 +24,11 @@ class ImageRepository extends Repository {
 
         const fetchImage = async () => {
             try {
-                const response = await fetch(url);
+                const response = await fetch(url, {
+                    headers: {
+                        'Cache-Control': 'public, max-age=31536000',
+                    }
+                });
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }

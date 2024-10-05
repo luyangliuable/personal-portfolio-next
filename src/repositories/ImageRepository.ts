ea5e261bb3e02f860b6762f9b40ad24015e75a9f
Luyang Liu <luyang.l@proton.me>
2024-10-05 17:09:22 +1000

    Improve code

--- src/repositories/ImageRepository.ts
+++ src/repositories/ImageRepository.ts
@@ -16,12 +16,10 @@ class ImageRepository extends Repository {
     async getImageById(idOrUrl: string, compression?: number): Promise<string> {
         if (idOrUrl === null) console.error("no image id provided");
         let url: string = idOrUrl;
-
         if (!idOrUrl.startsWith('http://') && !idOrUrl.startsWith('https://') && !idOrUrl.startsWith('/static')) url = `${ImageRepository.BASE_URL}${idOrUrl}`;
         url = `${url}?compression=${compression ?? 100}`;
         if (this.cache.has(url)) return this.cache.get(url)!;
         if (this.ongoingRequests.has(url)) return this.ongoingRequests.get(url)!;
-
         const fetchImage = async () => {
             try {
                 const response = await fetch(url, {

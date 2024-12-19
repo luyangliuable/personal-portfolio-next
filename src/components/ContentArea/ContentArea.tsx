aacded7da444d240efb76cb3c0c5d66d6dffd26d
Luyang Liu <luyang.l@proton.me>
2024-12-19 22:44:18 +1100

    Mitigate footer shower on navbar issue

--- src/components/ContentArea/ContentArea.tsx
+++ src/components/ContentArea/ContentArea.tsx
@@ -1,10 +1,11 @@
 "use server";
+import "./ContentArea.css";
 
 import ContentAreaHandler from "./ContentAreaHandler/ContentAreaHandler";
 
 const ContentArea: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="mb-[var(--footer-height)] bg-[var(--background-color)] box-shadow-lg">
+        <div className="content-area mb-[var(--footer-height)] bg-[var(--background-color)] box-shadow-lg">
             <ContentAreaHandler />
             {children}
         </div>

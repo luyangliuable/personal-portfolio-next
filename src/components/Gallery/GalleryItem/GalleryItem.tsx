4efb0ec2392b61d322164d897bfc1ef5b02e8f0c
luyangliuable <luyang.l@proton.me>
2026-03-16 00:31:08 +1100

    Reduce GalleryItem calculation adjustment to 0.35

--- src/components/Gallery/GalleryItem/GalleryItem.tsx
+++ src/components/Gallery/GalleryItem/GalleryItem.tsx
@@ -119,7 +119,7 @@ const GalleryItem: React.FC<IGalleryItemProps> = (props) => {
                         <div className="w-full box-border">
                             <SequentialRiseSpan
                                 minNumberOfLettersPerLine={42}
-                                calculationAdjustment={0.50}
+                                calculationAdjustment={0.35}
                             >
                                 {truncateTextBody(props.description, 200)}
                             </SequentialRiseSpan>
